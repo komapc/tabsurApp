181f13cfcf6553217959538a91418cf0bdf27017
mark <komapc@gmail.com>
2020-11-18 01:59:15 +0200

    fix upload file

--- app/src/main/java/com/dining_philosophers/beMyGuest/MainActivity.kt
+++ app/src/main/java/com/dining_philosophers/beMyGuest/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.dining_philosophers.beMyGuest
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import com.dining_philosophers.coolanu.R
+import java.util.logging.Logger
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,15 +23,22 @@ class MainActivity : AppCompatActivity() {
         webSettings.supportMultipleWindows()
         window.decorView.systemUiVisibility = 0x10
 
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url)
-                return true
-            }
-
-
-        }
         webView.loadUrl("https://www.BeMyGuest.app")
-    }
 
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: FileChooserParams
+            ): Boolean {
+                val LOG = Logger.getLogger(this.javaClass.name)
+
+                LOG.warning("onShowFileChooser")
+
+                return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+            }
+        }
+    }
 }
+
